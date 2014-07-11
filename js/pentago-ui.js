d0bf517cc176322a697be1a1b21bfb576e572227
Jake Newby <janewby@indiana.edu>
2014-07-11 11:26:32 -0400

    player vars

--- js/pentago-ui.js
+++ js/pentago-ui.js
@@ -1,13 +1,19 @@
 /*
-File Name: pentagoui.js
+File Name: pentago-ui.js
 Author: Jake Newby
 */
 
+// return output of pentagoui as a global JavaScript object(s)
 var pentagoui = (function(document, window){
 
   // html5 canvas and 2Dimensional canvas context
   var canvas = document.getElementById("gameCanvas");
   var context = null;
+
+  // the player data
+  var players = {};
+  players.player1 = {};
+  players.player2 = {};
 
   // check if the canvas and the context have rendered properly
   if (canvas)
@@ -29,6 +35,31 @@ var pentagoui = (function(document, window){
     return false;
   }
 
+  // ask for player names
+  var p1name = document.getElementById("player1name");
+  var p2name = document.getElementById("player2name");
+
+  // set player names & ids to objects
+  if (p1name)
+  {
+    if (p2name)
+    {
+      players.player2.id = 1;
+      players.player2.name = p2name;
+    }
+    else
+    {
+      // player2 name not found
+    }
+    players.player1.id = 0;
+    players.player1.name = p1name;
+
+  }
+  else
+  {
+    // player1 name not found
+  }
+
   // set size and color of board
   context.fillStyle = "#FFF";
   context.fillRect(0,0,500,500);
@@ -37,5 +68,8 @@ var pentagoui = (function(document, window){
   context.strokeStyle = 'black';
   context.stroke();
   context.lineWidth = 3;
+
+  //export the players object
+  return players;
 
 }(document, window));
